4486f4861881603004e434760816affd7a5c8ebc
ltabis <tab.rysak70@gmail.com>
2020-11-16 22:31:47 +0900

    :fire: cleaned the solution.

--- CG1/src/main.cpp
+++ CG1/src/main.cpp
@@ -2,17 +2,8 @@
 // Application entry point.
 
 #include <glm/gtx/string_cast.hpp>
-
 #include "Callbacks.hpp"
 #include "Profiling/Breakpoint.hpp"
-#include "Tests/TestBackgroundColorChange.hpp"
-#include "Tests/TestShapeAbstraction.hpp"
-#include "Tests/TestShapeSpawner.hpp"
-#include "Tests/TestDrawTriangle.hpp"
-#include "Tests/TestDrawCube.hpp"
-#include "Tests/TestCamera.hpp"
-#include "Tests/TestNoClipCameraController.hpp"
-#include "Tests/Framework/TestMenu.hpp"
 
 int main(void)
 {
@@ -20,21 +11,13 @@ int main(void)
 
 	std::shared_ptr<CG::Renderer> renderer = std::make_shared<CG::Renderer>("Test framework", 1920, 1080);
 	std::shared_ptr<CG::GUI> gui = std::make_shared<CG::GUI>(renderer->window(), CG::GUI::Style::DARK);
-	CG::Test::TestMenu menu(renderer, gui);
 
 	renderer->registerKeyBindingCallback(GLFW_KEY_ESCAPE, escape_callback);
 	renderer->registerKeyBindingCallback(GLFW_KEY_SPACE, spacebar_callback);
 
-	menu.registerTest<CG::Test::TestBackgroundColorChange>("Change color of background");
-	menu.registerTest<CG::Test::TestDrawTriangle>("Display a single colored triangle");
-	menu.registerTest<CG::Test::TestDrawCube>("Draw a colored cube");
-	menu.registerTest<CG::Test::TestShapeAbstraction>("Draw a colored plane from shape abstraction");
-	menu.registerTest<CG::Test::TestShapeSpawner>("Spawn multiple shapes");
-	menu.registerTest<CG::Test::TestCamera>("Test a camera transformation");
-	menu.registerTest<CG::Test::TestNoClipCameraController>("Camera controller");
+	while (!renderer->windowShouldClose()) {
 
-	while (!renderer->windowShouldClose())
-		menu.onRender();
+	}
 
 	return 0;
 }
