aad2d6d287259bcf9f6733d4971154c8e4e94f9f
ltabis <tab.rysak70@gmail.com>
2020-11-18 10:22:35 +0900

    :arrow_up: adaptation of the size of the squares proportional to the number of planes.

--- CG1/CheckeredFloor.cpp
+++ CG1/CheckeredFloor.cpp
@@ -4,7 +4,13 @@ CG::CheckeredFloor::CheckeredFloor(int size, int nsquare)
 	: m_Size	{ size    }
 	, m_Nsquare { nsquare }
 {
-	float squareSize = m_Size / m_Nsquare;
+	float minX = -static_cast<float>(size) / 2, minZ = -static_cast<float>(size) / 2;
+	float maxX =  static_cast<float>(size) / 2, maxZ =  static_cast<float>(size) / 2;
+
+	float sizeX = (maxX - minX) / static_cast<float>(m_Nsquare);
+	float sizeZ = (maxZ - minZ) / static_cast<float>(m_Nsquare);
+
+	float squareSize = static_cast<float>(m_Size) / static_cast<float>(m_Nsquare);
 
 	CG_LOG_INFO("number of squares: {} x {}", squareSize, squareSize);
 	CG_LOG_INFO("square size: {}", squareSize);
@@ -12,7 +18,7 @@ CG::CheckeredFloor::CheckeredFloor(int size, int nsquare)
 	for (int x = 0; x < m_Nsquare; ++x) {
 		for (int z = 0; z < m_Nsquare; ++z) {
 			_planes.push_back(std::make_unique<Plane>(
-				glm::vec3(x * 3, 0.f, z * 3),
+				glm::vec3(x * sizeX, 0.f, z * sizeZ),
 				glm::vec3(90.f, 0.f, 0.f),
 				glm::vec3(squareSize, 0.f, squareSize)
 			));
