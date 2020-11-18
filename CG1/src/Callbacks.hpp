ec315b062136e2eb7f3f9b95e321a961ae3a0b9c
ltabis <tab.rysak70@gmail.com>
2020-11-18 10:15:22 +0900

    :pencil: added documentation to the main code.

--- CG1/src/Callbacks.hpp
+++ CG1/src/Callbacks.hpp
@@ -7,9 +7,3 @@ static void escape_callback(CG::Renderer* renderer, int key, int scancode, int a
     if (action == GLFW_PRESS)
         glfwSetWindowShouldClose(renderer->window(), GLFW_TRUE);
 }
-
-static void spacebar_callback(CG::Renderer* renderer, int key, int scancode, int action, int mods)
-{
-    //if (action == GLFW_PRESS)
-    //    CG_CONSOLE_INFO("spacebar pressed");
-}
--- CG1/src/main.cpp
+++ CG1/src/main.cpp
@@ -9,13 +9,16 @@ int main(void)
 {
 	CG::Logger::Init();
 
+	// Creating the renderer and GUI elements.
 	CG::Renderer renderer("Checker board", 1920, 1080);
 	CG::GUI gui(renderer.window(), CG::GUI::Style::DARK);
-	CG::CheckeredFloor floor(50, 20);
+
+	// Creating the checker board with its shaders.
+	CG::CheckeredFloor floor(50, 16);
 	CG::ShaderLoader blueCheckerShader;
 	CG::ShaderLoader lightBlueCheckerShader;
-	CG::ShaderLoader cubeShader;
 
+	// Creating the camera controller.
 	CG::NoClipCameraController controller(
 		renderer.window(),
 		glm::vec3(0.f, 0.f, 1.f),
@@ -23,12 +26,15 @@ int main(void)
 	);
 	controller.speed = 0.1f;
 
+	// Creating a cube with it's shader.
 	std::unique_ptr<CG::AShape> cube = std::make_unique<CG::Cube>(
 		glm::vec3(0.f, 1.f, -3.f),
 		glm::vec3(0.f),
 		glm::vec3(1.f)
 	);
+	CG::ShaderLoader cubeShader;
 
+	// laoding shaders.
 	blueCheckerShader.load("./res/shaders/checker.shader");
 	blueCheckerShader.attach("triangle");
 	blueCheckerShader.attach("color_blue");
@@ -44,9 +50,10 @@ int main(void)
 	cubeShader.attach("color");
 	cubeShader.createExecutable();
 
+	// assigning callbacks for the renderer.
 	renderer.registerKeyBindingCallback(GLFW_KEY_ESCAPE, escape_callback);
-	renderer.registerKeyBindingCallback(GLFW_KEY_SPACE, spacebar_callback);
-
+	
+	// useful variables for frame independent code and fps mode for the controller.
 	float deltaTime = 0.f;
 	float lastFrame = 0.f;
 	bool _fpsMode = false;
@@ -61,11 +68,12 @@ int main(void)
 
 		controller.update(deltaTime);
 
+		// cleaning the screen and starting new frame for imgui.
 		renderer.pollEvents();
 		gui.newFrame();
 		renderer.clear();
 
-		// updating the fps mode.
+		// updating the fps mode if mouse clicked.
 		if (glfwGetMouseButton(renderer.window(), GLFW_MOUSE_BUTTON_2) == GLFW_PRESS) {
 			_fpsMode = !_fpsMode;
 			if (_fpsMode)
@@ -82,6 +90,7 @@ int main(void)
 
 				int idx = it + z;
 
+				// rendering one of two plane with a different color.
 				if (i % 2 == 1) {
 					blueCheckerShader.setUniform("u_mvp", controller.view() * board[idx]->transform.model());
 					renderer.draw(*(board[idx]), blueCheckerShader);
@@ -96,6 +105,7 @@ int main(void)
 		cubeShader.setUniform("u_mvp", controller.view() * cube->transform.model());
 		renderer.draw(*cube, cubeShader);
 
+		// draw all elements from the gui.
 		gui.drawDebugUI();
 		gui.renderGUI();
 		renderer.swapBuffers();
